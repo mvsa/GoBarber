176a43b65dea27d646a0705fea1fd8af6224faff
Marcos Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-08-15 18:21:46 -0300

    adding ContextApi with localstorage

--- src/context/AuthContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-interface AuthContextState {
-  name: string;
-}
-
-const AuthContext = createContext<AuthContextState>({} as AuthContextState); // hack para quando preciso passa o valor inicial como vazio
-
-export default AuthContext;
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,53 @@
+import React, { createContext, useCallback, useState } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextState {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextState>(
+  {} as AuthContextState,
+); // hack para quando preciso passa o valor inicial como vazio
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@Gobarber:token');
+    const user = localStorage.getItem('@Gobarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', { email, password });
+
+    const { token } = response.data;
+    const user = response.data.userWithoutPassword;
+
+    localStorage.setItem('@Gobarber:token', token);
+    localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
